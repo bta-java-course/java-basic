354af153fe076b14a7e1e57e1ce586c7531ae0a2
rubin008 <prubin008@outlook.com>
2020-06-19 17:58:12 +0300

    pr1

---
+++ src/buseduc/javacourse/prax/p01/Prax_01_pr.java
@@ -0,0 +1,114 @@
+public class Prax_01_pr {
+
+    public static void main(String[] args) {
+        /**
+         * Задание 1
+         * a=b=t=1
+         */
+
+        /**
+         * Задание 2  The variable  'y'  is declared two times.
+         */
+
+        /**
+         * Задание 3
+         */
+
+        int x = 2020;
+        System.out.println(x);
+
+        /**
+         * Задание 4
+         */
+
+        boolean a, b;
+        a = true;
+        b = false;
+
+        /**
+         * Задание 5
+         */
+        char ch1, ch2;
+        ch1 = 119;
+        ch2 = 'w';
+        System.out.println(ch1 + " +  " + ch2);
+
+        char ch17, ch27;
+        ch17 = 119;
+        ch27 = 'w';
+        char ch37=43;
+
+        System.out.println(ch17 + "  "+ ch37 +"  " + ch27);
+
+
+        char ch11, ch21;
+        ch11 = 119;
+        ch21 = 'w';
+        System.out.println( (int) ch11 + (int) ch21);
+
+
+        /**
+         * Задание 6
+         */
+
+        int x4, y4, z4, k4;
+        x4 = 119;
+        y4 = 0167;
+        z4 = 0x77;
+        k4 = 0B01110111;
+        System.out.println(x4 + " " + y4 + " " + z4 + " " + k4);
+
+
+
+        int x41, y41, z41, k41;
+        x41 = 119;
+        y41 = 0167;
+        z41 = 0x77;
+        k41 = 0B01110111;
+        System.out.println((x4 +1) + " " + (y4+1) + " " + (z4+1) + " " + (k4+1));
+
+
+
+
+        /**
+         * Задание 7
+         */
+
+        final int MY_CONST = 10;
+        int tmp;
+        tmp = MY_CONST + 10;
+        System.out.println(tmp);
+
+        /**
+         * Задание 8
+         */
+
+         byte y1 = 1, y2 = 2;
+         y1 =(byte)  ( (int) y1 + (int) y2);
+
+        System.out.println(y1);
+
+
+        short z1 = 1;
+        short z2 = 2;
+        z1 = (short)  ( (int) z1 + (int) z2);
+
+        System.out.println(z1);
+
+
+
+        /**
+         * Задание 9    The same as in the task 8.
+         */
+
+
+
+        /**
+         * Задание 10
+         */
+
+        int xx = 10, y = 3;
+        System.out.println( (float) xx / y );
+
+    }
+}
